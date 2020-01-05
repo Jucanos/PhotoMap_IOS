9cf05aed6251475571e112a4e121fad6a7668d08
김근수 <kimgeunsu@gimgeunsuui-MacBook-Pro.local>
2020-01-05 14:58:36 +0900

    [기능] 프로젝트 Init

---
+++ PhotoMap_IOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoMap_IOS
+//
+//  Created by 김근수 on 2020/01/05.
+//  Copyright © 2020 김근수. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("PhotoMap Project Init")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
