6161d08379055073a46961dbbb45529e6cf58675
김근수 <kimgeunsu@gimgeunsuui-MacBook-Pro.local>
2020-02-14 17:17:33 +0900

    [기능] Group Row에 대표지도를 표시할 star image 추가
    1. Group Row 우측 중단에 star image 추가
    2. 해당 group mid == primary 일때 표시

--- PhotoMap_IOS/Views/Group/GroupRow.swift
+++ PhotoMap_IOS/Views/Group/GroupRow.swift
@@ -12,10 +12,23 @@ struct GroupRow: View {
     var group: MapData
     var body: some View {
         HStack{
-            URLImage(URL(string: "https://s3.soybeans.tech/uploads/\(group.mid!)/main.png")!, expireAfter: Date(timeIntervalSinceNow: 0.1)){ proxy in
-                proxy.image
-                    .resizable()
-                    .frame(width: 50, height: 50)
+            ZStack {
+                URLImage(URL(string: "https://s3.soybeans.tech/uploads/\(group.mid!)/main.png")!, expireAfter: Date(timeIntervalSinceNow: 0.1)){ proxy in
+                    proxy.image
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                }
+                
+                //                ZStack {
+                //                    Circle()
+                //                        .frame(width: 20, height: 20)
+                //                        .foregroundColor(.red)
+                //                    Text("1")
+                //                        .foregroundColor(.white)
+                //                }
+                //                .frame(width: 15, height: 15)
+                //                .foregroundColor(.red)
+                //                .offset(x: 20, y: -20)
             }
             VStack(alignment: .leading) {
                 Text(verbatim: "\(group.name!)")
@@ -25,12 +38,18 @@ struct GroupRow: View {
             }
             .padding(5)
             Spacer()
+            
+            Image(systemName: "star.fill")
+                .resizable()
+                .frame(width: 20, height: 20)
+                .foregroundColor(.yellow)
+                .opacity(UserSettings.shared.userInfo?.data?.primary == group.mid ? 1 : 0)
         }
     }
 }
 
 //struct GroupRow_Previews: PreviewProvider {
-//    static var tgroup = UserGroup(name: "test", updateTime: "2020.01.01", imageName: "uir")
+//    static var tgroup = MapData(name: "Test", updatedAt: "2020.01.01")
 //
 //    static var previews: some View {
 //        Group{
