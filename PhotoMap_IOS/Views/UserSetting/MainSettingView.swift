3c7caab69eeddd49eb4e3db10c7b655fa2e4c457
김근수 <kimgeunsu@gimgeunsuui-MacBook-Pro.local>
2020-02-18 18:07:58 +0900

    [리팩토링] Setting View layout 변경 완료
    각 Row에 Icon 추가

--- PhotoMap_IOS/Views/UserSetting/MainSettingView.swift
+++ PhotoMap_IOS/Views/UserSetting/MainSettingView.swift
@@ -7,29 +7,32 @@
 //
 
 import SwiftUI
+import URLImage
 
 struct MainSettingView: View {
     var body: some View {
         VStack {
             UserInfoView()
+                .padding()
             List {
                 Section(header: Text("앱 정보")) {
                     NavigationLink(destination: Notice()) {
-                        SettingRow(settingName: "공지사항")
+                        SettingRow(setImage: "exclamationmark.bubble", settingName: "공지사항")
                     }
-                    SettingRow(settingName: "리뷰 작성하기")
-                    SettingRow(settingName: "앱 공유하기")
+                    SettingRow(setImage: "ellipses.bubble",settingName: "리뷰 작성하기")
+                    SettingRow(setImage: "square.and.arrow.up",settingName: "앱 공유하기")
                 }
                 Section(header: Text("회원정보관리")) {
-                    SettingRow(settingName: "비밀번호 변경")
+                    SettingRow(setImage: "lock",settingName: "비밀번호 변경")
                     NavigationLink(destination: UserLogout()) {
-                        SettingRow(settingName: "로그아웃")
+                        SettingRow(setImage: "person.crop.circle.badge.exclam",settingName: "로그아웃")
                     }
                     NavigationLink(destination: UserWithdrawal()) {
-                        SettingRow(settingName: "회원탈퇴")
+                        SettingRow(setImage: "person.crop.circle.badge.xmark",settingName: "회원탈퇴")
                     }
                 }
-            }.listStyle(GroupedListStyle())
+            }
+            .listStyle(GroupedListStyle())
         }
     }
     
@@ -37,41 +40,52 @@ struct MainSettingView: View {
 
 
 struct SettingRow: View {
+    @State var setImage: String
     @State var settingName: String
     var body: some View {
-        Text("\(settingName)")
+        HStack{
+            Image(systemName: setImage)
+                .resizable()
+                .frame(width: 20, height: 20)
+                .foregroundColor(Color(appColor))
+            Text("\(settingName)")
+        }
+        
     }
 }
 
 struct UserInfoView: View {
     var body: some View {
         HStack {
-            Image(systemName: "rectangle.stack.person.crop.fill")
-                .resizable()
-                .frame(width: 50, height: 50)
-            Text("test0000@gmail.com")
-                .font(.headline)
+            URLImage(URL(string: (UserSettings.shared.userInfo?.data?.thumbnail!)!)!) { proxy in
+                proxy.image
+                    .resizable()
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .frame(width: 50, height: 50)
+            }
+            Text((UserSettings.shared.userInfo?.data?.nickname!)!)
+                .font(.title)
             
             Spacer()
         }
-        .padding(5)
     }
 }
 
 struct MainSettingView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
-            MainSettingView()
-                .previewDevice(PreviewDevice(rawValue: "iPhone SE"))
-                .previewDisplayName("iPhone SE")
-            
-            MainSettingView()
-                .previewDevice(PreviewDevice(rawValue: "iPhone 8"))
-                .previewDisplayName("iPhone 8")
-            
-            MainSettingView()
-                .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro"))
-                .previewDisplayName("iPhone 11 Pro")
-        }
+//        Group {
+//            MainSettingView()
+//                .previewDevice(PreviewDevice(rawValue: "iPhone SE"))
+//                .previewDisplayName("iPhone SE")
+//
+//            MainSettingView()
+//                .previewDevice(PreviewDevice(rawValue: "iPhone 8"))
+//                .previewDisplayName("iPhone 8")
+//
+//            MainSettingView()
+//                .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro"))
+//                .previewDisplayName("iPhone 11 Pro")
+//        }
+        UserInfoView()
     }
 }
