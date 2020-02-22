108cbb78441a03ab37a39335ca2afc109c5838ef
김근수 <kimgeunsu@gimgeunsuui-MacBook-Pro.local>
2020-02-22 21:41:58 +0900

    [리팩토링] Add Group layout 변경
    그룹 추가창 layout 개선 및 animation 변경

--- PhotoMap_IOS/Views/Group/AddGroup.swift
+++ PhotoMap_IOS/Views/Group/AddGroup.swift
@@ -19,7 +19,6 @@ struct AddGroup: View {
                     EmptyView()
                 }
                 .background(Color.black.opacity(0.7))
-                .opacity(self.isOpen ? 1.0 : 0.0)
                 .onTapGesture {
                     self.menuClose()
                     self.endEditing()
@@ -27,13 +26,16 @@ struct AddGroup: View {
                 
                 VStack {
                     SubAddGroup(isLoading: self.$isLoading, menuClose: self.menuClose)
-                        .frame(height: 100)
+                        .frame(height: 90)
                         .background(Color(appColor))
-                        .offset(y: self.isOpen ? 0 : -UIScreen.main.bounds.height)
-                        .animation(.easeInOut(duration: 0.4))
+                        //                        .offset(y: self.isOpen ? 0 : -UIScreen.main.bounds.height)
+                        .offset(y: 0)
+                    EmptyView().frame(height: 30)
                     Spacer()
                 }
             }
+            .opacity(self.isOpen ? 1.0 : 0.0)
+            .animation(.default)
             .edgesIgnoringSafeArea(.bottom)
         }
         
@@ -52,10 +54,11 @@ struct SubAddGroup: View {
     let menuClose: () -> Void
     var body: some View {
         
-        VStack(spacing: 0) {
+        VStack(alignment: .leading, spacing: 5) {
             Text("그룹 추가하기")
-                .font(.title)
+                .font(.system(size: 25, weight: .heavy))
                 .foregroundColor(.white)
+                .padding(.leading, 15)
             HStack{
                 TextField("그룹 이름을 입력하세요", text: $groupName)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
@@ -72,13 +75,13 @@ struct SubAddGroup: View {
                         }
                     }
                 }) {
-                    Image(systemName: "plus.square")
+                    Image(systemName: "checkmark.circle")
                         .resizable()
+                        .scaledToFit()
                         .frame(width: 30, height: 30)
                         .foregroundColor(.white)
                 }
-            }
-        .padding()
+            }.padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
         }
         .alert(isPresented: $showAlert){
             Alert(title: Text("그룹 이름이 없습니다"), message: Text("그룹 이름을 정한 후 추가해주세요!"), dismissButton: .default(Text("취소")))
@@ -91,5 +94,11 @@ struct SubAddGroup: View {
 extension UIApplication {
     func endEditing() {
         sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+struct SubAddGroup_Previews: PreviewProvider {
+    static var previews: some View {
+        SubAddGroup(isLoading: .constant(false), menuClose: {})
     }
 }
