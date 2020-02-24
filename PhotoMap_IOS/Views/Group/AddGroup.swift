6550fc61b59d69e9e23ff9d83022756acb65f492
김근수 <kimgeunsu@gimgeunsuui-MacBook-Pro.local>
2020-02-24 18:48:25 +0900

    [버그] Group add시 name 잔존 해결
    Group add 창 나갈때, 완료했을 때 이전 name 남아있는 현상 해결

--- PhotoMap_IOS/Views/Group/AddGroup.swift
+++ PhotoMap_IOS/Views/Group/AddGroup.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct AddGroup: View {
     @State var isLoading: Bool = false
+    @State var groupName: String = ""
     let isOpen: Bool
     let menuClose: () -> Void
     var body: some View {
@@ -20,15 +21,15 @@ struct AddGroup: View {
                 }
                 .background(Color.black.opacity(0.7))
                 .onTapGesture {
+                    self.groupName = ""
                     self.menuClose()
                     self.endEditing()
                 }
                 
                 VStack {
-                    SubAddGroup(isLoading: self.$isLoading, menuClose: self.menuClose)
+                    SubAddGroup(isLoading: self.$isLoading, groupName: self.$groupName, menuClose: self.menuClose)
                         .frame(height: 90)
                         .background(Color(appColor))
-                        //                        .offset(y: self.isOpen ? 0 : -UIScreen.main.bounds.height)
                         .offset(y: 0)
                     EmptyView().frame(height: 30)
                     Spacer()
@@ -48,12 +49,11 @@ struct AddGroup: View {
 struct SubAddGroup: View {
     @EnvironmentObject var userSettings: UserSettings
     @ObservedObject var userGroupStore = UserGroupStore.shared
-    @State var groupName: String = ""
     @State var showAlert: Bool = false
     @Binding var isLoading: Bool
+    @Binding var groupName: String
     let menuClose: () -> Void
     var body: some View {
-        
         VStack(alignment: .leading, spacing: 5) {
             Text("그룹생성")
                 .font(.system(size: 25, weight: .heavy))
@@ -70,6 +70,7 @@ struct SubAddGroup: View {
                         self.isLoading = true
                         self.userGroupStore.addMap(name: self.groupName){
                             self.isLoading = false
+                            self.groupName = ""
                             self.menuClose()
                             self.endEditing()
                         }
@@ -94,11 +95,5 @@ struct SubAddGroup: View {
 extension UIApplication {
     func endEditing() {
         sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-    }
-}
-
-struct SubAddGroup_Previews: PreviewProvider {
-    static var previews: some View {
-        SubAddGroup(isLoading: .constant(false), menuClose: {})
     }
 }
